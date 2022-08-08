4d3fa1d58bb21808565c228ce647c7b0575a5701
Yurii <yurakupchunsky@gmail.com>
2022-08-08 14:53:05 +0300

    Added a check for count value

--- Task_1/Js/script.js
+++ Task_1/Js/script.js
@@ -20,4 +20,15 @@ let question_1 = prompt("What was yhe last movie you watched?", "");
 let question_2 = prompt("How do you appreciate this film", "");
 personalMovieDB.movies["a"] = question_1;
 personalMovieDB.movies["b"] = question_2;
+
+if (personalMovieDB.count < 10) {
+  alert("You watched a small number of films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("You are typical viewer");
+} else if (personalMovieDB.count > 30) {
+  alert("You are movie buff)");
+} else {
+  alert("Something went wrong!");
+}
+
 console.log(personalMovieDB);
